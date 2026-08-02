8d7a3e75410d15f7d04af55d1e192af2d2909d40
arijitbh20 <arijitbubunbhandari@gmail.com>
2026-08-02 22:19:34 +0530

    Restructure

--- app/core/config.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-FASTAPI_URL = os.getenv("FASTAPI_URL")
-DASHBOARD_URL = os.getenv("DASHBOARD_URL")
-DATABASE_PATH = os.getenv("DATABASE_PATH")
